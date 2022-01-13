ff3fa9f7a068b8da30cd1a9ae0d2248b2d8cda24
Diego Alejandro Sadras <diegosadras@yahoo.com.ar>
2022-01-13 20:28:16 -0300

    1

--- heroku/index.js
+++ heroku/index.js
@@ -33,54 +33,6 @@ app.get(['/facebook', '/instagram'], function (req, res) {
 
 app.post('/facebook', function (req, res) {
     console.log('Facebook request body:', req.body);
-    /*
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
-  */
-    /*
-    const separator = process.env.SEPARATOR || '|';
-    const arrEntry = req.body.entry || req.body[0].entry;
-    //for (let i = 0; i < arrEntry.length; ++i) {
-    const i = 0;
-        const entry_time = arrEntry[i].time;
-        const arrChanges = arrEntry[i].changes
-        //for (let c = 0; c < arrChanges.length; ++c) {
-            const c = 0;
-            if (arrChanges[c].field === 'comments') {
-                const myValue = arrChanges[c].value;
-                const post_id = myValue.post_id;
-                const comment_id = myValue.comment_id;
-                var comment_message = '';
-                var user_id = '';
-                var user_name = '';
-                var peso = '1';
-                if (myValue.verb === 'delete')
-                    peso = '-1';
-                else {
-                    if (myValue.verb === 'edit')
-                        peso = '0';
-                    user_id = myValue.from.id;
-                    user_name = myValue.from.name;
-                    comment_message = myValue.message;
-                };
-                const contentLine = post_id.concat(separator, entry_time, separator, comment_id,
-                    separator, comment_message, separator, user_id, separator, user_name, separator,
-                    peso, '\n');
-                //fs.appendFile('./diego.log', contentLine, err => {
-                //    if (err) {
-                //        console.error(err)
-                //        return
-                //    }
-                //    //done!
-                //});
-                res.send(contentLine);
-            };
-        //};
-    //};
-    */
     // Process the Facebook updates here
     received_updates.unshift(req.body);
     console.log('Acumulado:', JSON.stringify(received_updates, null, 2) );
